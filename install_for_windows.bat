e58c44d695a3f70c8c0acf16163660e83d0409ce
Ayman Othmane <ayman.othmane@dauphine.eu>
2024-01-21 23:15:16 +0100

    Set up all

--- install_for_windows.bat
+++ install_for_windows.bat
@@ -1,3 +1,4 @@
-python -m venv .venv
+C:\Users\ayman\AppData\Local\Microsoft\WindowsApps\python3.11 -m venv .venv
+.\.venv\Scripts\pip.exe install pip -U
 .\.venv\Scripts\pip.exe install -r requirements.txt
 
